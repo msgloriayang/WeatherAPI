1096e7f501d2f469d4c57efad343d7e0546e06f0
msgloriayang <yanggloria91@gmail.com>
2023-03-19 14:58:33 -0500

    added an array of cities

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-var APIKey = "eb253f48b78c5f91d425a9840bd0d476"
+var APIKey = "eb253f48b78c5f91d425a9840bd0d476";
+var city = ["Minneapolis", "Duluth", "Maple Grove"];
+fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
+  .then(response => response.json())
+  .then(data => console.log(data));
