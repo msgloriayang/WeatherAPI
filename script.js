fe7dbe824a4ad53b190851d940212084962c65e8
msgloriayang <yanggloria91@gmail.com>
2023-03-19 23:35:37 -0500

    added var cityInput in js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-var apiKey = "eb253f48b78c5f91d425a9840bd0d476";
-var city = "Minneapolis";
+var apiKey = `eb253f48b78c5f91d425a9840bd0d476`;
+var searchForm = document.querySelector("form");
+var cityInput = document.querySelector("#city-input");
+var searchHistory = document.querySelector('#search-history');
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}")
+// this is to retrieve the weather and have it show on index.html
+function getWeather(city) {
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
   .then(response => response.json())
   .then(data => {
+    const cityName = data.name;
     const temp = Math.round(data.main.temp - 273.15);
     const condition = data.weather[0].description;
     const location = data.name;
@@ -11,3 +16,38 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}
     weatherDiv.innerHTML = `The temperature in ${location} is ${temp}&deg;C with ${condition}.`;
   })
   .catch(error => console.log(error));
+}
+
+// this is where users can search for a city
+searchForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const city = cityInput.value.trim();
+    getWeather(city);
+  });
+
+//   this is to enable search history to show up in html
+  function saveSearchHistory(city) {
+    let searchHistory = localStorage.getItem("searchHistory");
+    if (!searchHistory) {
+      searchHistory = [];
+    } else {
+        searchHistory = JSON.parse(searchHistory);
+    }
+    searchHistory.push(city);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  }
+  
+  function loadSearchHistory() {
+    let searchHistory = localStorage.getItem("searchHistory");
+    if (searchHistory) {
+      searchHistory = JSON.parse(searchHistory);
+      const searchHistoryList = document.querySelector("#search-history");
+      for (const city of searchHistory) {
+        const li = document.createElement("li");
+        li.textContent = city;
+        searchHistoryList.appendChild(li);
+      }
+    }
+  }
+  
+  loadSearchHistory();
