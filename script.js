820b12442ae80eb3f49fdb19edf796da81d5da4e
msgloriayang <yanggloria91@gmail.com>
2023-03-19 21:04:13 -0500

    added flex box

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 var APIKey = "eb253f48b78c5f91d425a9840bd0d476";
 var city = ["Minneapolis", "Duluth", "Maple Grove"];
-var event
-
-const searchForm = document.querySelector('#search-form');
-
-searchForm.addEventListener('submit', function(event)) {
-    event.preventDefault();
-}
-
-const query = searchInput.value;
 
 fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
   .then(response => response.json())
   .then(data => console.log(data));
-
-  searchHistory.addEventListener('click', function(event) {
-  });
